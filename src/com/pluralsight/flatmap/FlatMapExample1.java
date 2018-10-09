9ffa4eb80797f037b534ed48203c65e0be5e410f
dmahapa2 <debasish.mahapatra2@t-mobile.com>
2018-10-09 04:23:27 -0700

    pluralsight concepts

--- src/com/pluralsight/flatmap/FlatMapExample1.java
+++ src/com/pluralsight/flatmap/FlatMapExample1.java
@@ -1,7 +1,44 @@
 package com.pluralsight.flatmap;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/*
+* How to merge large amount of text and cut it into words using flatmap()
+*
+* */
 public class FlatMapExample1 {
-    static public void main(String... args){
+    public static void main(String[] args) throws IOException {
+        Stream<String> stringStream1 = Files.lines(Paths.get("D:\\File\\TomSawer_01.txt"));
+        Stream<String> stringStream2 = Files.lines(Paths.get("D:\\File\\TomSawer_02.txt"));
+        Stream<String> stringStream3 = Files.lines(Paths.get("D:\\File\\TomSawer_03.txt"));
+        Stream<String> stringStream4 = Files.lines(Paths.get("D:\\File\\TomSawer_04.txt"));
+
+        /*System.out.println("stringStream1 count: "+stringStream1.count());
+        System.out.println("stringStream2 count: "+stringStream2.count());
+        System.out.println("stringStream3 count: "+stringStream3.count());
+        System.out.println("stringStream4 count: "+stringStream4.count());*/
+        //Stream.of takes varargs
+        Stream<Stream<String>> streamOfStreams = Stream.of(stringStream1, stringStream2, stringStream3, stringStream4);
+        //System.out.println("# of streams: "+streamOfStreams.count());
+        //Below code takes every stream and puts it in a big stream
+        //Stream<String> streamOfLines = streamOfStreams.flatMap(stream -> stream);
+        //Alternative of above .. Function.Identity means it takes the same element and returns the same
+        Stream<String> streamOfLINES = streamOfStreams.flatMap(Function.identity());
+        //System.out.println("# of lines: "+streamOfLines.count());
+        //System.out.println("# of lines: "+streamOfLINES.count());
+
+
+        Function<String,Stream<String>> lineSplitter = line -> Pattern.compile(" ").splitAsStream(line);
+        Stream<String> streamOfWords = streamOfLINES.flatMap(lineSplitter)
+                                                    .map(String::toLowerCase)
+                                                    .filter(word -> word.length() == 4)
+                                                    .distinct();
+        System.out.println("# of words in novel: "+streamOfWords.count());
 
     }
 }
---
+++ src/com/pluralsight/streamofnumbers/StreamOfNumbersExample1.java
@@ -0,0 +1,63 @@
+package com.pluralsight.streamofnumbers;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+
+/*
+* 1.Suppose we have a stream of people<Person object> and we want to average the ages of all the people whose age > 29
+* 2.But it(average) is available in IntStream
+* */
+public class StreamOfNumbersExample1 {
+    public static void main(String[] args) throws IOException {
+        Set<String> shakespeareWords =
+                Files.lines(Paths.get("D:\\File\\words.shakespeare.txt"))
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
+
+        Set<String> scrabbleWords =
+                Files.lines(Paths.get("D:\\File\\ospd.txt"))
+                .map(String::toLowerCase)
+                .collect(Collectors.toSet());
+
+        System.out.println("# of words of shakespere: "+shakespeareWords.size());
+        System.out.println("# of words of scrabble: "+scrabbleWords.size());
+
+        final int[] scrabbleEnScore = {
+//              a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p,  q, r, s, t, u, v, w, x, y,  z
+                1, 3, 3, 2, 1, 5, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10
+        };
+
+        Function<String,Integer> score =
+                word -> word.chars().map(letter -> scrabbleEnScore[letter - 'a']).sum();
+
+        ToIntFunction<String> intScore =
+                word -> word.chars().map(letter -> scrabbleEnScore[letter - 'a']).sum();
+
+        System.out.println("Score of \"hello\": "+intScore.applyAsInt("hello"));
+
+        String word =
+                shakespeareWords.stream()
+                .filter(word1 -> scrabbleWords.contains(word1))
+                .max(Comparator.comparing(score))
+                .get();
+
+        System.out.println("The best word is: "+word);
+
+        IntSummaryStatistics intSummaryStatistics = shakespeareWords.stream()
+                .filter(scrabbleWords::contains)
+                .mapToInt(intScore)
+                .summaryStatistics();
+
+        System.out.println("Status: "+intSummaryStatistics);
+
+
+
+    }
+}
